b881c180c2d62eab89e3b31d3296804b58c3fddc
Samual Norman <samualtnorman@gmail.com>
2022-09-30 20:39:28 +0100

    first commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "i'm Samual"
+export const SITE_DESCRIPTION = "i'm Samual and I like to make stuff"
