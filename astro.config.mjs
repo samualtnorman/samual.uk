264f1652901b8cbb607e921e0130660c73f2ad81
Samual Norman <samualtnorman@gmail.com>
2022-10-01 00:09:31 +0100

    less css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
-
 import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
-export default defineConfig({ site: "https://example.com", integrations: [mdx(), sitemap()] })
+export default defineConfig({
+	site: "https://samual.uk/",
+	integrations: [ mdx(), sitemap() ],
+	server: { port: 8080 }
+})
