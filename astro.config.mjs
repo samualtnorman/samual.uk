988f579e56979919e9f075927ae4603dfcad3ddf
Samual Norman <samualtnorman@gmail.com>
2023-07-10 23:38:59 +0100

    fix absolute paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import critters from "astro-critters"
+import relativeLinks from "astro-relative-links"
 
 export default /** @type {import("astro").AstroUserConfig} */ ({
 	site: `https://samual.uk/`,
-	integrations: [ mdx(), sitemap(), critters() ],
+	integrations: [ mdx(), sitemap(), critters(), relativeLinks() ],
 	server: { port: 8080, host: true },
 	trailingSlash: `always`
 })
