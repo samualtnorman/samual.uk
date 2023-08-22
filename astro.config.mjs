01a1153c44736fb7e1e22fb545adca3c0afd9b01
Samual Norman <me@samual.uk>
2023-08-22 21:05:46 +0100

    replace `astro-critters` with `@frontendista/astro-html-minify` critters breaks `<style>` in `<noscript>`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
-import critters from "astro-critters"
+import htmlMinify from "@frontendista/astro-html-minify"
 import relativeLinks from "astro-relative-links"
 
-export default /** @type {import("astro").AstroUserConfig} */ ({
+/** @type {import("astro").AstroUserConfig} */ export default {
 	site: `https://samual.uk/`,
-	integrations: [ mdx(), sitemap(), critters(), relativeLinks() ],
+	integrations: [
+		mdx(),
+		sitemap(),
+		relativeLinks(),
+		htmlMinify({
+			html: {
+				processConditionalComments: true,
+				removeAttributeQuotes: true,
+				removeComments: true,
+				removeEmptyAttributes: true,
+				removeOptionalTags: true,
+				removeTagWhitespace: true,
+				sortClassName: true,
+				useShortDoctype: true,
+				preserveLineBreaks: false,
+				conservativeCollapse: false,
+				collapseInlineTagWhitespace: false
+			}
+		})
+	],
 	server: { port: 8080, host: true },
 	trailingSlash: `always`
-})
+}
