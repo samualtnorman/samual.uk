45d37ebf66d37be4b9e10c21b52af24d814c92f8
Samual <me@samual.uk>
2025-05-05 13:38:22 +0100

    update site description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "I'm Samual"
-export const SITE_DESCRIPTION = "I'm Samual and I like to make stuff"
+export const SITE_DESCRIPTION = "I'm a software developer."
